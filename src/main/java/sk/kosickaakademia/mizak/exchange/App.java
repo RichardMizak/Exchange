a8f61f3e58b1d923cd395ba5546b0d81145be76a
RichardMizak <richard.mizak@kosickaakademia.sk>
2021-04-12 11:34:00 +0200

    .

--- src/main/java/sk/kosickaakademia/mizak/exchange/App.java
+++ src/main/java/sk/kosickaakademia/mizak/exchange/App.java
@@ -1,15 +1,26 @@
 package sk.kosickaakademia.mizak.exchange;
 
-
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 import java.util.HashSet;
 import java.util.Set;
 
-public class App{
+public class App extends Application{
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("GuiCalculator.fxml"));
+        primaryStage.setTitle("CurrencyExchanger3000");
+        primaryStage.setScene(new Scene(root, 400, 600));
+        primaryStage.show();
+    }
     public static void main(String[] args) {
         Set<String> set = new HashSet<>();
         set.add("USD");
 
+    }
 
-    }
-    }
+}
 
